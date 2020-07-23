6a1f88e78a41f20048487924552ae02d2dc97b0e
Hallboo <zhanghaobo@mipitalk.com>
2020-07-23 20:46:45 +0800

    Extract all feature for every dataset

--- run.sh
+++ run.sh
@@ -7,7 +7,9 @@ echo "Step:$step" && sleep 2;
 # CUDA_VISIBLE_DEVICES="3"
 
 if [ $step -eq 1 ]; then
-    for dataset in task1_dev  task1_dev_enroll  task1_enroll  task2_dev  task2_enroll  task3_enroll  train; do
+    #all_dataset="task1_dev  task1_dev_enroll  task1_enroll  task2_dev  task2_enroll  task3_enroll  train"
+    all_dataset="task2_dev  task2_enroll  task3_enroll  train"
+    for dataset in $all_dataset; do
         python preprocess.py data/$dataset data/$dataset/feats --num_workers=10
     done
 fi
