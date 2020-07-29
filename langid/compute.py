f5dfa42194096f395688e043331e03b5a1faa186
Hallboo <zhanghaobo@mipitalk.com>
2020-07-29 13:16:16 +0800

    Three score: Accuracy Cavg EER

---
+++ langid/compute.py
@@ -0,0 +1,84 @@
+import os, sys, re
+import numpy as np
+from sklearn.metrics import confusion_matrix
+import computeCavg as cavg
+import utils
+
+def ComputeAccuracy(all_outputs, all_predict, all_targets):
+
+    if not (all_predict.shape == all_targets.shape):
+        raise ValueError("The shape of all_predict and all_targets must be the same.")
+ 
+    # compute the confusion matrix
+    # TODO 未测试在有未知语言时，计算混淆矩阵是否正确。未知语言的target = -1
+    confu_mat = confusion_matrix(all_targets, all_predict)
+    confu_mat_norm = confu_mat.astype('float') / confu_mat.sum(axis=1)[:, np.newaxis]
+
+    class_total = np.sum(confu_mat, axis=1)
+    acc = np.trace(confu_mat) / np.sum(class_total)
+    # accuracy is the sum of confusion matrix diagonal divide total number of sample
+    # in evalset
+
+    class_acc = np.diagonal(confu_mat) / class_total
+    # accuracy for each class
+
+    return acc, class_acc, confu_mat
+
+def GetPair(all_outputs, all_targets):
+    pairs = []
+    lang_num = all_outputs.shape[1]
+    for u in range(len(all_outputs)):
+        for l in range(len(all_outputs[u])):
+            pairs.append([l, all_targets[u], all_outputs[u][l]])
+
+    min_score = np.min(all_outputs)
+    max_score = np.max(all_outputs)
+
+    return pairs, lang_num, min_score, max_score
+
+def ComputeCavg(all_outputs, all_targets):
+
+    pairs, lang_num, min_score, max_score = GetPair(all_outputs, all_targets) 
+    threshhold_bins = 20
+    p_target = 0.5
+    cavgs, min_cavg = cavg.get_cavg(pairs, lang_num, min_score, max_score, threshhold_bins, p_target)
+    print(round(min_cavg, 4))
+
+def ComputeEER(all_outputs, all_targets):
+    import subprocess
+    pair, _, _, _, = GetPair(all_outputs, all_targets)
+    score2target = ''
+
+    for it in pair:
+        lang  = it[0]
+        lalab = it[1]
+        score = it[2]
+        if lang == lalab:
+            score2target += "{}\ttarget\n".format(score)
+        else:
+            score2target += "{}\tnontarget\n".format(score)
+    score2target = score2target[:-1]
+
+    tempfile = '/tmp/olr-compute-eer-score'
+    with open(tempfile, 'w') as fp:
+        fp.write(score2target)
+
+    cmd = 'compute-eer {}'.format(tempfile).split()
+    pro = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    out, err = pro.communicate()
+
+    err = err.decode('utf-8').replace('\n', ' ')
+    pat = r".*Equal error rate is ([0-9\.]+)%, at threshold ([-0-9\.]+).*"
+    eer = float(re.sub(pat, r'\1', err))
+    thd = float(re.sub(pat, r'\2', err))
+    print(eer, thd)
+
+if __name__=="__main__":
+
+    all_outputs = np.load("egs/outputs.npy", allow_pickle=True)
+    all_targets = np.load("egs/targets.npy", allow_pickle=True)
+    all_predict = np.argmax(all_outputs, axis = 1)
+    ComputeAccuracy(all_outputs, all_predict, all_targets)
+    #ComputeCavg(all_outputs, all_predict, all_targets, 'data/dev_all/spk2utt', 'data/dev_all/utt2lang')
+    ComputeCavg(all_outputs, all_targets)
+    ComputeEER(all_outputs, all_targets)
